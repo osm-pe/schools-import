3a83a833e1d376cbbc4f16fa2f721f8fe81b6bcd
Rub21 <rub2106@gmail.com>
2018-05-09 16:04:39 -0500

    fixing iscedlevel tag

--- scripts/README.md
+++ scripts/README.md
@@ -1,13 +1,27 @@
 # Scripts to fix the imported data
 
-### Fix refs
+**Note**
+
+Don't use the script on large regions, this script should be managed carefully
+
+### Fixing ref tag
 
 ```
 python refs.py osmfile.osm
 
 ```
+
 The command line will return an osmfile_new.osm file, which you have to upload to osm.
 
-**Note**
 
-Don't use the script on large regions, this script should be managed carefully
+
+### Fixing isced:level tag
+
+```
+python isced-level.py osmfile.osm
+
+```
+
+The command line will return an osmfile_new.osm file, which you have to upload to osm.
+
+
---
+++ scripts/isced-level.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys
+import string
+from lxml import etree
+osmfilename = sys.argv[1] if (len(sys.argv) > 1) else sys.exit("Invalid file name")
+tree = etree.parse(osmfilename)
+iscedLevels = tree.findall(".//tag[@k='isced:level']");
+modify=False
+
+def rigthTags( arr ):
+  fixisced=[]
+  if arr[0]=='primary':
+    fixisced.append('1')
+    fixisced.append('2')
+  else:
+    fixisced.append('2')
+    fixisced.append('1')
+  return ';'.join(fixisced)
+
+def fixIsCedLevel( iscedLevel ):
+  iscedLevel=iscedLevel.replace(" ", "").lower()
+  a=iscedLevel.split(':')
+  b=iscedLevel.split(';')
+  if len(a)>1:
+    iscedLevel=rigthTags(a)
+  elif len(b)>1:
+    iscedLevel=rigthTags(b)
+  elif iscedLevel=='primary':
+    iscedLevel='1'
+  elif iscedLevel=='secondary':
+    iscedLevel='2'
+  return iscedLevel
+
+for iscedLevel in iscedLevels:
+  parent = iscedLevel.getparent()
+  amenity=parent.findall(".//tag[@k='amenity']")
+  if len(amenity) > 0 and (amenity[0].get("v").title()=='School' or amenity[0].get("v").title()=='Kindergarten'):
+    old_iscedLevels=iscedLevel.get("v").title();
+    new_iscedLevels=fixIsCedLevel(old_iscedLevels)
+    # print old_iscedLevels +' => '+ new_iscedLevels
+    if old_iscedLevels != new_iscedLevels:
+      print old_iscedLevels +' => '+ new_iscedLevels
+      iscedLevel.set("v", new_iscedLevels)
+      modify=True;
+  if modify == True :
+    parent.attrib['action']='modify'
+    modify = False
+xml = "<?xml version='1.0' encoding='UTF-8'?>\n"+etree.tostring(tree, encoding='utf8')
+new_file = open(osmfilename[:-4]+'_new'+osmfilename[-4:], 'w')
+new_file.write(xml)
